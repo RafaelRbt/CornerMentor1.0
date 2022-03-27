c6ae6acea7ce0fbe3e486ad36761d2c23a22d4d1
Humberto Carrillo <A01377246@tec.mx>
2022-03-26 21:58:36 -0600

    Añado archivos iniciales

---
+++ app/src/main/java/mx/itesm/TeamRocket_CornerMentor/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.itesm.TeamRocket_CornerMentor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
